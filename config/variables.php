1fc51cc7af68e576a771d56a42c95fa8abeaa23d
Emine Mutlu <e.mutlu@necron.com.tr>
2019-07-11 12:10:46 +0300

    +

--- config/variables.php
+++ config/variables.php
@@ -3,10 +3,9 @@
 
     $file_name='netas_dev';
     $url = 'http://localhost:8070/'.$file_name;
+    $google_API_key = '';
 
-    $google_API_key=''
-
-    $page=@$_GET['page']; 
-    $page_title="";
+    $type_arr = ['airport','art_gallery','atm','bank','bar','beauty_salon','bus_station','book_store','cafe','car_wash','dentist
+','gym','hospital','museum','park','school'];
 
   ?>
